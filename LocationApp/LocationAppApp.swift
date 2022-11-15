520a3302777a3ef7ccd5b1123ada3c46e66ca5ae
khaled hussien <57367756+khaled2024@users.noreply.github.com>
2022-11-16 01:43:31 +0200

    Initial Commit

---
+++ LocationApp/LocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAppApp.swift
+//  LocationApp
+//
+//  Created by KhaleD HuSsien on 16/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct LocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
